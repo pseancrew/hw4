786804b12c11a039c36649e12dc118b241a2a1e1
pseancrew <sean.paquet@kellogg.northwestern.edu>
2022-03-14 00:40:06 +0000

    adds logout and pass formatting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :current_user
 
   def current_user
-    puts "------------------ code before every request ------------------"
+    if session["user.id"]
+    @current_user = User.find(session["user.id"])
   end
 end
