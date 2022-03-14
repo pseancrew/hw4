a5416c7b1d195577b14b01dd53ee217adb757bd9
pseancrew <sean.paquet@kellogg.northwestern.edu>
2022-03-14 00:56:39 +0000

    fixes conditional logoout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,9 @@ class ApplicationController < ActionController::Base
   before_action :current_user
 
   def current_user
-    if session["user.id"]
-    @current_user = User.find(session["user.id"])
+    @current_user = User.find(session[:user_id]) 
+    if session[:user_id]
+    end
   end
+
 end
