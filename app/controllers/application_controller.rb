71a1708f703f5b0fc351ef44cd66019549b8457c
pseancrew <sean.paquet@kellogg.northwestern.edu>
2022-03-15 02:02:32 +0000

    adds user authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :current_user
 
   def current_user
-    @current_user = User.find(session[:user_id]) 
-    if session[:user_id]
-    end
+    @current_user = User.find(session[:user_id]) if session[:user_id]
   end
 
 end
